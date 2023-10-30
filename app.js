195e6c53b8ef3512ca4aabbeb05143574afb4542
AldaCeleste <aceleste.paez46@gmail.com>
2023-10-30 16:26:05 -0300

    Generacion de estructura del proyecto, con servidor Express

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require ('express');
+const path = require ('path');
+const app = express();
+
+const publicFolderPath = path.resolve(__dirname, './public');
+app.use(express.static(publicFolderPath));
+
+app.listen(3000,()=> {
+    console.log("Servidor corriendo en el puerto 3000");
+});
+
+app.get('/', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, './views/home.html'))
+});
+app.get('/productDetail', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, './views/productDetail.html'))
+});
+app.get('/productCart', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, './views/productCart.html'))
+});
+app.get('/login', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, './views/login.html'))
+});
+app.get('/register', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, './views/register.html'))
+});
