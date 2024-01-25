edcf42c8853fe8c3ea8d5bfffd9679e99a0a1505
Marcos Quinteros <104270568+M4gnus99@users.noreply.github.com>
2024-01-25 07:16:45 -0300

    Se modifico la vista de productoDetail,
    Se agrego un carrusel y los iconos de lo que incluye el paquete
    Se quito los ítems de no incluye y e itinerario momentáneamente

--- src/controllers/productDetailController.js
+++ src/controllers/productDetailController.js
@@ -6,17 +6,37 @@ const fs = require('fs');
 const productsFilePath = path.join(__dirname, '../data/products.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
+const obtenerIcono = (opcion) => {
+  // Lógica para asociar opciones con iconos
+  // Puedes personalizar esto según tus necesidades
+  const iconos = {
+    'Asistencia al Viajero':'fa-user-doctor',
+    'Hoteles 5 estrellas': 'fa-hotel',
+    'Coordinador y guias locales': 'fa-user',
+    'Régimen de comidas todo incluido': 'fa-utensils',
+    'Excursiones según programa': 'fa-map-location-dot',
+    'Traslados de ingreso y egreso': 'fa-bus',
+  };
+
+  return iconos[opcion] || 'fa-question'; // 'fa-question' para opciones sin icono asociado
+};
+
 const productDetailController = {
     index:(req, res) => {
         const productId = req.params.id;
         const product = products.find(product => product.id_producto === Number(productId));
-        
+        // Asocia opciones con iconos
+        const incluyeConIconos = product.incluye.map(opcion => ({
+          opcion: opcion,
+          icono: obtenerIcono(opcion),
+        }));
         res.render('./products/productDetail.ejs',
          { stylesheetPath: '/css/productDetail.css',
         //  producto especifico
           product: product,
         products: products,
-        usuarioLogueado: req.session.usuarioLogueado });  //todos los productos
+        usuarioLogueado: req.session.usuarioLogueado,
+        incluyeConIconos: incluyeConIconos, });  //todos los productos
     }
 }
 
